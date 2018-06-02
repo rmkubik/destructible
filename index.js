bbafeb7e38acdaa3aa45951e6f4484e20ada527a
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-06-01 20:39:37 -0700

    Run prettier

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import { h, app } from 'hyperapp';
+import { h, app } from "hyperapp";
 
-import './src/css/main.scss';
+import "./src/css/main.scss";
 
 const initialState = {
-  count: 0,
+    count: 0
 };
 
 const actionFns = {
-  down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value }),
+    down: value => state => ({ count: state.count - value }),
+    up: value => state => ({ count: state.count + value })
 };
 
 const view = (state, actions) => (
-  <div>
-    <h1>{state.count}</h1>
-    <button onclick={() => actions.down(1)}>-</button>
-    <button onclick={() => actions.up(1)}>+</button>
-  </div>
+    <div>
+        <h1>{state.count}</h1>
+        <button onclick={() => actions.down(1)}>-</button>
+        <button onclick={() => actions.up(1)}>+</button>
+    </div>
 );
 
 app(initialState, actionFns, view, document.body);
