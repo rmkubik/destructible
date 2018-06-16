e2213fbc4966794d5dcd56df7564708bac896ddb
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-06-15 17:54:51 -0700

    Make blocks collideable

--- src/js/game/Player.js
+++ src/js/game/Player.js
@@ -13,20 +13,27 @@ class Player extends Phaser.GameObjects.Sprite {
     this.sKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.dKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     scene.input.keyboard.on('keydown_SPACE', () => this.placeBlock(scene));
+
+    scene.terrain.setCollision([204]);
+    scene.physics.add.collider(this, scene.terrain, () => {
+      console.log('collided with terrain layer');
+    });
   }
 
   update() {
+    this.body.setVelocity(0);
+
     if (this.wKey.isDown) {
-      this.y -= 5;
+      this.body.setVelocityY(-200);
     }
     if (this.aKey.isDown) {
-      this.x -= 5;
+      this.body.setVelocityX(-200);
     }
     if (this.sKey.isDown) {
-      this.y += 5;
+      this.body.setVelocityY(200);
     }
     if (this.dKey.isDown) {
-      this.x += 5;
+      this.body.setVelocityX(200);
     }
   }
 
