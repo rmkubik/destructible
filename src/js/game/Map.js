4f85b8d80dfaf185896121c61ba2ca3e5b1b2541
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-06-19 23:03:50 -0700

    Grow grass in map cells with adjacent map cell

--- src/js/game/Map.js
+++ src/js/game/Map.js
@@ -2,21 +2,29 @@ import Phaser from 'phaser';
 
 class Map {
   constructor(scene) {
+    this.grassTileList = [0, 1, 2, 3];
+    this.width = 12;
+    this.height = 12;
+
     this.map = scene.make.tilemap({
-      data: Map.createMapData(),
+      data: this.createMapData(),
       tileWidth: 64,
       tileHeight: 64,
       tileSpacing: 10,
     });
+
     const tileset = this.map.addTilesetImage('tiles', 'tiles', 64, 64, 0, 10);
     this.ground = this.map.createDynamicLayer(0, tileset, 0, 0);
     this.terrain = this.map.createBlankDynamicLayer(1, tileset);
-    this.width = 12;
-    this.height = 12;
-    this.grassTileList = [0, 1, 2, 3];
+
+    this.growGrass();
+    this.growGrass();
+    this.growGrass();
+    this.growGrass();
+    this.growGrass();
   }
 
-  static createMapData() {
+  createMapData() {
     const dirtTileList = [4, 5];
     const mapData = [];
 
@@ -40,17 +48,28 @@ class Map {
     return mapData;
   }
 
-  static growGrass(currentMapData) {
-    const newMapData = [];
+  growGrass() {
+    const newGrassTiles = [];
     for (let y = 0; y < this.height; y += 1) {
-      const row = [];
       for (let x = 0; x < this.width; x += 1) {
-        const tileIndex = Phaser.Math.RND.weightedPick(dirtTileList);
-        row.push(tileIndex);
+        const neighbors = this.getNeighbors({ x, y });
+        if (
+          neighbors.some(({ index }) =>
+            this.grassTileList.some(grassTileIndex => grassTileIndex === index),
+          )
+        ) {
+          newGrassTiles.push({ x, y });
+        }
       }
-      newMapData.push(row);
     }
-    return newMapData;
+
+    newGrassTiles.forEach(({ x, y }) => {
+      this.ground.putTileAt(
+        Phaser.Math.RND.weightedPick(this.grassTileList),
+        x,
+        y,
+      );
+    });
   }
 
   static isTileInMap(tile, mapData) {
@@ -58,7 +77,7 @@ class Map {
     return x >= 0 && x < mapData[0].length && y >= 0 && y < mapData.length;
   }
 
-  static getNeighbors(tile, mapData) {
+  getNeighbors(tile) {
     const neighbors = [];
 
     const up = { x: tile.x, y: tile.y - 1 };
@@ -66,17 +85,17 @@ class Map {
     const left = { x: tile.x - 1, y: tile.y };
     const right = { x: tile.x + 1, y: tile.y };
 
-    if (Map.isTileInMap(up, mapData)) {
-      neighbors.push(mapData[up.y][up.x]);
+    if (this.ground.getTileAt(up.x, up.y)) {
+      neighbors.push(this.ground.getTileAt(up.x, up.y));
     }
-    if (Map.isTileInMap(down, mapData)) {
-      neighbors.push(mapData[down.y][down.x]);
+    if (this.ground.getTileAt(down.x, down.y)) {
+      neighbors.push(this.ground.getTileAt(down.x, down.y));
     }
-    if (Map.isTileInMap(left, mapData)) {
-      neighbors.push(mapData[left.y][left.x]);
+    if (this.ground.getTileAt(left.x, left.y)) {
+      neighbors.push(this.ground.getTileAt(left.x, left.y));
     }
-    if (Map.isTileInMap(right, mapData)) {
-      neighbors.push(mapData[right.y][right.x]);
+    if (this.ground.getTileAt(right.x, right.y)) {
+      neighbors.push(this.ground.getTileAt(right.x, right.y));
     }
 
     return neighbors;
