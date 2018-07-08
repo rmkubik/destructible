cca691545237eb9186f7bc3c171ee893eae9c234
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-07 21:04:27 -0700

    Implement wander fsm and mob inherit prefab

--- src/js/game/Mob.js
+++ src/js/game/Mob.js
@@ -1,25 +1,18 @@
 import Phaser from 'phaser';
 
-import Fsm from './fsm';
+import Fsm from './Fsm';
+import Prefab from './Prefab';
 
-class Mob extends Phaser.GameObjects.Sprite {
+class Mob extends Prefab {
   constructor({ scene, position, sprite }) {
-    super(scene, position.x, position.y, 'animals', `${sprite}.png`);
-    scene.physics.world.enable(this);
-    scene.add.existing(this);
+    super({ scene, position, sheet: 'animals', sprite });
 
-    this.setPosition(this.width / 2, this.height / 2);
     this.setScale(0.25);
 
     scene.physics.add.collider(this, scene.map.terrain);
 
-    this.direction = Mob.pickRandomDirection();
+    this.direction = [0, 0];
     this.speed = 100;
-
-    const angle = Mob.convertVelocityToAngle(this.direction);
-
-    this.body.setVelocityX(this.speed * Math.cos(angle / (180 / Math.PI)));
-    this.body.setVelocityY(this.speed * Math.sin(angle / (180 / Math.PI)));
 
     this.body.setCollideWorldBounds(true);
     this.body.setBounce(1, 1);
@@ -27,17 +20,46 @@ class Mob extends Phaser.GameObjects.Sprite {
     this.state = new Fsm(
       {
         unmoving: {
-          wander: () => {},
+          wander: () => {
+            // pick random direction
+            this.direction = Mob.pickRandomDirection();
+            const angle = Mob.convertVelocityToAngle(this.direction);
+
+            this.body.setVelocityX(
+              this.speed * Math.cos(angle / (180 / Math.PI)),
+            );
+            this.body.setVelocityY(
+              this.speed * Math.sin(angle / (180 / Math.PI)),
+            );
+
+            scene.time.addEvent({
+              // pick random amount of time to wander
+              delay: Phaser.Math.RND.integerInRange(1000, 5000),
+              callback: () => {
+                // transition back to unmoving
+                this.state.action('stop');
+              },
+            });
+            this.state.transition('wandering');
+          },
         },
         wandering: {
-          stop: () => {},
+          stop: () => {
+            this.body.setVelocity(0);
+            this.state.transition('unmoving');
+          },
         },
       },
       'unmoving',
     );
+    // this.state.action('wander');
   }
 
-  update() {}
+  update() {
+    if (this.state.currentState === 'unmoving') {
+      this.state.action('wander');
+    }
+  }
 
   static pickRandomDirection() {
     return {
