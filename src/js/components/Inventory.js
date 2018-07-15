de96cfcf8c1dbe7686b3398bebcb085e8d7312e6
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-15 11:11:15 -0700

    Bold selected item slot

--- src/js/components/Inventory.js
+++ src/js/components/Inventory.js
@@ -3,7 +3,11 @@ import { h } from 'hyperapp';
 import '../../css/components/Inventory.scss';
 
 const Inventory = ({ state }) => {
-  const items = state.inventory.items.map(item => <li>{item}</li>);
+  const items = state.inventory.items.map((item, index) => (
+    <li class={state.inventory.selectedItemIndex === index ? 'selected' : ''}>
+      {item}
+    </li>
+  ));
   return <ul class="Inventory">{items}</ul>;
 };
 
