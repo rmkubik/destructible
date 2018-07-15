5f5e9bf9abd612e3051e73db5a08f7b82da7232f
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-14 20:10:56 -0700

    Look up state plugin index instead of hardcoding

--- src/js/components/GameComponent.js
+++ src/js/components/GameComponent.js
@@ -5,15 +5,19 @@ import Game from '../game/Game';
 import '../../css/components/Game.scss';
 
 let game;
+let statePluginIndex;
 
 const GameComponent = ({ actions, state }) => (
   <div
     id="Game"
     oncreate={() => {
       game = new Game(actions, state);
+      statePluginIndex = game.game.plugins.plugins.findIndex(
+        plugin => plugin.key === 'HyperappState',
+      );
     }}
     onupdate={() => {
-      game.game.plugins.plugins[1].plugin.update(state);
+      game.game.plugins.plugins[statePluginIndex].plugin.update(state);
     }}
   />
 );
