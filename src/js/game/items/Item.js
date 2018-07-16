2d5cb0aea69c3ddaec91db673671e58a4b8e4828
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-15 20:42:48 -0700

    Error statement in default Item use fn

--- src/js/game/items/Item.js
+++ src/js/game/items/Item.js
@@ -4,15 +4,7 @@ class Item {
   }
 
   use() {
-    console.log(this.type);
-
-    // console.error(
-    //   `Item: ${
-    //     this.scene.state.inventory.items[
-    //       this.scene.state.inventory.selectedItemIndex
-    //     ].type
-    //   } use function is not implemented`,
-    // );
+    console.error(`Item "${this.type}" use function is not implemented`);
   }
 }
 
